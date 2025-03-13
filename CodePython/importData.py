7f78cc355c03541fed537000fa11c4aac0212165
KarimAmeur <karim.ameur.Ap@gmail.com>
2025-03-13 13:30:56 +0200

    MiseEnLigne11

---
+++ CodePython/importData.py
@@ -0,0 +1,12 @@
+import pandas as pd
+def load_data(file_path):
+    # Chemin du fichier
+    file_path = file_path
+
+    # Lecture du fichier dans un DataFrame
+    df = pd.read_csv(file_path, delimiter=',')  # Utilisez '\t' si les colonnes sont séparées par des tabulations
+    df = df.drop(columns=['customer_id'])
+    # Affichage des premières lignes du DataFrame pour vérifier
+    print(df.head())
+
+    return df
