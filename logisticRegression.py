a9b3eb82f8e2da678b469a936f5f2631ee581508
KarimAmeur <karim.ameur.Ap@gmail.com>
2025-03-13 14:10:44 +0200

    NouvelleBase

--- logisticRegression.py
+++ logisticRegression.py
@@ -5,7 +5,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
 
-
 def logistic_regression(df):
     """
     Effectue une régression logistique sur le DataFrame pour prédire la colonne 'default' et 
@@ -14,53 +13,42 @@ def logistic_regression(df):
     Args:
         df (pd.DataFrame): DataFrame contenant les données.
     """
-    # Séparation des features (X) et de la target (y)
     X = df.drop(columns=['default'])
     y = df['default']
 
-    # Séparation en ensembles d'entraînement et de test (80% train, 20% test)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Initialisation du modèle
-    model = LogisticRegression()
+    model_lr = LogisticRegression()
 
     with mlflow.start_run():
-        # Entraînement du modèle
-        model.fit(X_train, y_train)
+        model_lr.fit(X_train, y_train)
 
-        # Prédictions
-        y_pred = model.predict(X_test)
+        y_pred = model_lr.predict(X_test)
 
-        # Calcul des métriques
         accuracy = accuracy_score(y_test, y_pred)
         report = classification_report(y_test, y_pred, output_dict=True)
-        
-        # Enregistrer les hyperparamètres
+
         mlflow.log_param("solver", "liblinear")
         mlflow.log_param("max_iter", 100)
-        
-        # Enregistrement des métriques
+
         mlflow.log_metric("accuracy", accuracy)
         mlflow.log_metric("precision", report['accuracy'])
         mlflow.log_metric("recall", report['weighted avg']['recall'])
         mlflow.log_metric("f1_score", report['weighted avg']['f1-score'])
 
-        # Enregistrement de la matrice de confusion
         cm = confusion_matrix(y_test, y_pred)
         mlflow.log_text(str(cm), "confusion_matrix.txt")
 
-        # Enregistrement du modèle
-        mlflow.sklearn.log_model(model, "logistic_regression_model")
-        
-        # Enregistrement d'un tag pour l'expérience
+        mlflow.sklearn.log_model(model_lr, "logistic_regression_model")
+
         mlflow.set_tag("experiment_type", "logistic_regression")
-        
-        # Affichage des résultats
+
         print("Accuracy:", accuracy)
         print("\nMatrice de confusion:")
         print(cm)
         print("\nRapport de classification:")
         print(report)
+    return model_lr
 
 
 def random_forest(df):
@@ -71,53 +59,40 @@ def random_forest(df):
     Args:
         df (pd.DataFrame): DataFrame contenant les données.
     """
-    # Séparation des features (X) et de la target (y)
-    X = df.drop(columns=['default'])  # Toutes les colonnes sauf 'default'
-    y = df['default']  # La colonne 'default' est la variable cible
+    X = df.drop(columns=['default'])
+    y = df['default']
 
-    # Séparation en ensembles d'entraînement et de test (80% train, 20% test)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Initialisation du modèle Random Forest avec 100 arbres
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model_rf = RandomForestClassifier(n_estimators=100, random_state=42)
 
     with mlflow.start_run():
-        # Entraînement du modèle sur l'ensemble d'entraînement
-        model.fit(X_train, y_train)
+        model_rf.fit(X_train, y_train)
 
-        # Prédiction sur l'ensemble de test
-        y_pred = model.predict(X_test)
+        y_pred = model_rf.predict(X_test)
 
-        # Calcul des métriques
         accuracy = accuracy_score(y_test, y_pred)
         report = classification_report(y_test, y_pred, output_dict=True)
         cm = confusion_matrix(y_test, y_pred)
 
-        # Enregistrement des hyperparamètres
         mlflow.log_param("n_estimators", 100)
         mlflow.log_param("random_state", 42)
-        
-        # Enregistrement des métriques
+
         mlflow.log_metric("accuracy", accuracy)
         mlflow.log_metric("precision", report['accuracy'])
         mlflow.log_metric("recall", report['weighted avg']['recall'])
         mlflow.log_metric("f1_score", report['weighted avg']['f1-score'])
 
-        # Enregistrement de la matrice de confusion
         mlflow.log_text(str(cm), "confusion_matrix.txt")
-
-        # Enregistrement du rapport de classification
         mlflow.log_text(str(report), "classification_report.txt")
 
-        # Enregistrement du modèle
-        mlflow.sklearn.log_model(model, "random_forest_model")
-        
-        # Enregistrement d'un tag pour l'expérience
+        mlflow.sklearn.log_model(model_rf, "random_forest_model")
+
         mlflow.set_tag("experiment_type", "random_forest")
-        
-        # Affichage des résultats
+
         print("Accuracy:", accuracy)
         print("\nMatrice de confusion:")
         print(cm)
         print("\nRapport de classification:")
         print(report)
+    return model_rf
---
+++ test.py
@@ -0,0 +1,31 @@
+import importData as id
+import logisticRegression as lr
+import pandas as pd
+
+# Chemin du fichier de données
+file_path = '/Users/karimameur/Documents/ProjetMLOps/Loan_Data.csv'
+    
+ # Charger les données dans un DataFrame
+df = id.load_data(file_path)
+print("commit3")
+# Exécuter les modèles
+print("Exécution de la régression logistique...")
+model=lr.logistic_regression(df)
+
+# Nouvelle donnée pour la prédiction
+new_data = pd.DataFrame([{
+    'customer_id' : 43210,
+    'credit_lines_outstanding': 5,
+    'loan_amt_outstanding': 60000,
+    'total_debt_outstanding': 100000,
+    'income': 90000,
+    'years_employed': 8,
+    'fico_score': 680
+}])
+
+# Prédiction avec le modèle entraîné
+prediction = model.predict(new_data)
+
+# Vérification du résultat attendu
+assert prediction[0] == 1, "Incorrect prediction"
+print("Test réussi : la prédiction est correcte.")
